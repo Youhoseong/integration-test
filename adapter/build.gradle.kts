b60609daff1aa8736ad54519bcf9d4ce69d50656
Youhoseong <h0song@naver.com>
2023-10-22 13:50:22 +0900

    Refactor as multimodule project

---
+++ adapter/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.0")
+    implementation(project(":domain"))
+    implementation(project(":usecase"))
+}
---
+++ domain/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
